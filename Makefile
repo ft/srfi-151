cafb11a8cc41bd13a24945386c8a388b9864bcd3
Frank Terbeck <ft@bewatermyfriend.org>
2020-01-07 21:02:09 +0100

    Initial commit
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ Makefile
@@ -0,0 +1,55 @@
+TOPDIR = .
+
+LOAD_PATH = $(TOPDIR)/scheme
+TEST_PATH = $(TOPDIR)/test
+GUILE_BINARY ?= guile
+GUILE_CALL = $(GUILE_BINARY) -L $(LOAD_PATH) -C $(LOAD_PATH) --no-auto-compile
+GUILD_BINARY ?= guild
+
+CFLAGS = -Wunsupported-warning -Wunused-variable -Wunused-toplevel
+CFLAGS += -Wunbound-variable -Warity-mismatch -Wduplicate-case-datum
+CFLAGS += -Wbad-case-datum -Wformat -L$(LOAD_PATH)
+
+COMPILE = $(GUILD_BINARY) compile $(CFLAGS)
+MODULES = $(TOPDIR)/scheme/srfi/srfi-151.scm
+OBJECTS = ${MODULES:.scm=.go}
+
+TESTS = $(TOPDIR)/test/*.t
+RUNTESTS = $(TOPDIR)/tools/run-single-test
+PROVE = prove --merge --color -e '$(RUNTESTS)'
+INSTALL = $(GUILE_CALL) $(TOPDIR)/tools/install
+
+.SUFFIXES: .scm .go
+
+all:
+	@echo
+	@echo " Available targets:"
+	@echo
+	@echo "           all: This help text"
+	@echo "       install: Install scheme modules to system paths"
+	@echo "       compile: Byte-compile the client library"
+	@echo "         clean: Remove byte-compiled scheme code"
+	@echo "          test: Run test suite"
+	@echo "  test-verbose: Run test suite (with verbose test harness)"
+	@echo "    test-debug: Run test suite (With all debugging enabled)"
+	@echo
+
+.scm.go:
+	$(COMPILE) -o $@ $<
+
+compile: $(OBJECTS)
+
+clean:
+	find . -name "*.go" -exec rm -f '{}' +
+	find . -name "*~" -exec rm -f '{}' +
+
+install:
+	$(INSTALL)
+
+test:
+	$(PROVE) $(TESTS)
+
+test-verbose:
+	$(PROVE) --verbose $(TESTS)
+
+.PHONY: all compile clean install test test-verbose
---
+++ scheme/srfi/srfi-151.scm
@@ -0,0 +1,128 @@
+;; SRFI 151: Bitwise Operations
+;; https://srfi.schemers.org/srfi-151/
+
+(define-module (srfi srfi-151)
+  #:use-module ((srfi srfi-60) #:prefix srfi60:))
+
+;; Basic Operations
+
+(define-public bitwise-not lognot)
+(define-public bitwise-and logand)
+(define-public bitwise-ior logior)
+(define-public bitwise-xor logxor)
+
+(define-public bitwise-eqv
+  (case-lambda
+    (() -1)
+    ((a) a)
+    ((a b) (bitwise-not (bitwise-xor a b)))
+    ((a b . rest) (apply bitwise-eqv (cons (bitwise-eqv a b) rest)))))
+
+(define-public (bitwise-nand . lst)
+  (bitwise-not (apply bitwise-and lst )))
+
+(define-public (bitwise-nor . lst)
+  (bitwise-not (apply bitwise-ior lst )))
+
+(define (complement-a fnc a b)
+  (fnc (bitwise-not a) b))
+
+(define (complement-b fnc a b)
+  (fnc a (bitwise-not b)))
+
+(define-public (bitwise-andc1 a b)
+  (complement-a bitwise-and a b))
+
+(define-public (bitwise-andc2 a b)
+  (complement-b bitwise-and a b))
+
+(define-public (bitwise-orc1 a b)
+  (complement-a bitwise-ior a b))
+
+(define-public (bitwise-orc2 a b)
+  (complement-b bitwise-ior a b))
+
+;; Integer Operations
+
+(define-public arithmetic-shift ash)
+(define-public bit-count logcount)
+
+;; Guile has integer-length implemented in C in the default environment.
+
+;;(define (integer-length x)
+;;  (inexact->exact (ceiling (/ (log (if (negative? x)
+;;                                       (- x)
+;;                                       (+ 1 x)))
+;;                              (log 2)))))
+
+(define-public bitwise-if srfi60:bitwise-if)
+
+;; Single-Bit Operations
+
+(define-public bit-set? logbit?)
+(define-public copy-bit srfi60:copy-bit)
+
+(define-public (bit-swap n1 n2 i)
+  (copy-bit n2
+            (copy-bit n1 i (bit-set? n2 i))
+            (bit-set? n1 i)))
+
+(define-public any-bit-set? logtest)
+
+(define-public (every-bit-set? test b)
+  (= test (logand test b)))
+
+(define-public first-set-bit srfi60:first-set-bit)
+
+;; Bit Field Operations
+
+(define-public bit-field bit-extract)
+
+(define-public (bit-field-any? x start end)
+  (not (zero? (bit-field x start end))))
+
+(define-public (bit-field-every? x start end)
+  (= (bit-field x start end)
+     (bit-field-set 0 0 (- end start))))
+
+(define-public (bit-field-set x start end)
+  (let ((n (- end start)))
+    (bitwise-ior x (arithmetic-shift (1- (ash 1 n)) start))))
+
+(define-public (bit-field-clear x start end)
+  (bitwise-and x (bitwise-not (bit-field-set 0 start end))))
+
+(define-public (bit-field-replace dst src start end)
+  (bitwise-ior (bit-field-clear dst start end)
+               (arithmetic-shift (bit-field src 0 (- end start)) start)))
+
+(define-public (bit-field-replace-same dst src start end)
+  (bitwise-ior (bit-field-clear dst start end)
+               (bitwise-and src (bit-field-set 0 start end))))
+
+(define-public bit-field-rotate srfi60:rotate-bit-field)
+(define-public bit-field-reverse srfi60:reverse-bit-field)
+
+;; Bits Conversion
+
+(define-public bits->list srfi60:integer->list)
+(define-public bits->vector
+  (case-lambda
+    ((x) (list->vector (bits->list x)))
+    ((x len) (list->vector (bits->list x len)))))
+
+(define-public list->bits srfi60:list->integer)
+(define-public (vector->bits v)
+  (list->bits (vector->list v)))
+
+(define-public (bits . lst)
+  (list->bits (reverse lst)))
+
+;; Fold, Unfold, and Generate
+
+(define-public (bitwise-fold proc seed x)
+  (let loop ((count-down (integer-length x)) (rest x) (seed seed))
+    (if (zero? count-down)
+        seed
+        (loop (1- count-down) (arithmetic-shift rest -1)
+              (proc (not (zero? (bitwise-and rest 1))) seed)))))
